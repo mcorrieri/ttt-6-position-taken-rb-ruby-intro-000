5b5a0885c5ac6116f4f4fbbe01cd984766236cb4
Michael Corrieri <mlcorrieri@gmail.com>
2021-02-08 22:22:17 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,28 +1,28 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  else
-    return true
-  end
-
-end
-
-
-
-
-
-
-
-
-
-#   if board[index] == " "
-#    false
-#   elsif board[index] == ""
-#    false
-#   elsif board[index == nil]
-#    false
+#   if board[index] == " " || board[index] == "" || board[index] == nil
+#     return false
 #   else
-#    true
+#     return true
 #   end
+#
 # end
+
+
+
+
+
+
+
+
+
+  if board[index] == " "
+   false
+  elsif board[index] == ""
+   false
+  elsif board[index == nil]
+   false
+  else
+   true
+  end
+end
